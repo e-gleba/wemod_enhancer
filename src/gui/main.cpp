da3ff8719d307bac736f12ce8bcc6dc01880815d
Evgeniy Gleba <60469435+e-gleba@users.noreply.github.com>
2026-09-01 23:39:31 +0300

    remove invalid placeholder

--- src/gui/main.cpp
+++
@@ -1,9 +0,0 @@
-// WeMod Enhancer - desktop app (Dear ImGui + SDL3, C++23).
-// Linux font rendering fix: force SDL renderer texture creation after backend init.
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <imgui.h>
-#include <imgui_impl_sdl3.h>
-#include <imgui_impl_sdlrenderer3.h>
-
-int main_placeholder = 0;
